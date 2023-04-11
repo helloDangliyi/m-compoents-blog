63584932e14d5bcd31e85da1e688500803a3f258
DDang-L <1968806729@qq.com>
2023-04-11 10:15:06 +0800

    基础搭建

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+//配置
+module.exports= {
+    // 网站标题
+    title:'基于element-plus二次封装组件',
+    // 部署的基础路径
+    base:'/',
+    // 配置网站的头部
+    head:[],
+    // 主题配色
+    themeConfig:{
+        // 头部导航
+        nav:[
+            {
+                // 导航名称
+                text:'首页'
+            }
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroText: 基于element-plus二次封装组件
+tagline: 高扩展性的组件库
+actionText: 快速开始
+actionLink: //
+features:
+  - title: 简介至上
+    details: 所有组件支持全量引入和按需引入，并且用法简单
+  - title: 高扩展性
+    details: 全新的组件api设计，支持高度自定义
+  - title: 全面覆盖
+    details: 涵盖基础组件、通用组件和业务组件
+---
